75ce0c54bd30d147cb72acf18f5474cac2e6c86e
superballGitHub <taweesinmini@gmail.com>
2015-10-19 09:52:44 +0700

    Commit first version
    Commit first version  of moodle phase II By Taweesin

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mysqli';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'moodle2900p2';
+$CFG->dbuser    = 'admin';
+$CFG->dbpass    = 'admin';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => '',
+  'dbsocket' => '',
+);
+
+$CFG->wwwroot   = 'http://localhost:8080/moodle_PhaseII';
+$CFG->dataroot  = 'C:\\xampp\\moodledata29p2';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(dirname(__FILE__) . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
